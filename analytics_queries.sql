431e45d5a8ef005b4133ea82af43d68fe52d0357
Jeremy Nassar <70663426+jeremynassar@users.noreply.github.com>
2024-10-01 14:00:00 -0400

    Update analytics_queries.sql

--- analytics_queries.sql
+++ analytics_queries.sql
@@ -7,26 +7,30 @@ order by 2 desc
 limit 5
 
 
--- This query is looking for most viewed title by page views by wikipedia site
+-- This query is looking for the most viewed title by page views for each wikipedia site
 WITH base AS (
-  select
-    title
-    , wiki
-    , sum(views) as total_page_views
-  from `bigquery-public-data.wikipedia.pageviews_2023`
-  where DATE(datehour) >= "2023-01-01"
-  group by 1,2
+    select
+        title,
+        wiki,
+        sum(views) as total_page_views
+    from `bigquery-public-data.wikipedia.pageviews_2023`
+    where DATE(datehour) >= "2023-12-30"
+    group by title, wiki
+),
+
+ranked_pages AS (
+    select 
+        title,
+        total_page_views as views,
+        wiki,
+        row_number() over (partition by wiki order by total_page_views desc) as search_ranking
+    from base
 )
 
-select * from (
-  select title 
-        ,total_page_views as views
-        , wiki
-        , row_number() over(partition by wiki order by total_page_views desc) as search_ranking
-  from base 
-)
+select *
+from ranked_pages
 where search_ranking = 1
-order by 3
+order by wiki
 
 
   -- This query is giving us a aggregated look at the sum of views to all wikipedia sites by month
